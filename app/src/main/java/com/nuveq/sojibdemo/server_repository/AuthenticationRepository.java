927b7717a3da90d57a289a53cc83dd2612213aa8
mdmasum.shuvo <mdmasum.shuvo.cse@gmail.com>
2019-10-17 14:04:11 +0600

    profile and add ateendance

--- app/src/main/java/com/nuveq/sojibdemo/server_repository/AuthenticationRepository.java
+++ app/src/main/java/com/nuveq/sojibdemo/server_repository/AuthenticationRepository.java
@@ -91,10 +91,6 @@ public class AuthenticationRepository {
     }
 
 
-
-
-
-
     public void setCallbackListener(ServerResponseFailedCallback mListener) {
         this.mListener = mListener;
     }
