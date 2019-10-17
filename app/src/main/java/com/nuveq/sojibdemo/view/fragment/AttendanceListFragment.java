0630df33e15671122b758249e6eb6023a1265a21
mdmasum.shuvo <mdmasum.shuvo.cse@gmail.com>
2019-10-17 18:46:21 +0600

    profile and add ateendance

--- app/src/main/java/com/nuveq/sojibdemo/view/fragment/AttendanceListFragment.java
+++ app/src/main/java/com/nuveq/sojibdemo/view/fragment/AttendanceListFragment.java
@@ -1,9 +1,18 @@
 package com.nuveq.sojibdemo.view.fragment;
 
+import android.content.Intent;
+import android.os.Handler;
+import android.widget.Toast;
+
 import com.nuveq.sojibdemo.R;
 import com.nuveq.sojibdemo.common.BaseFragment;
+import com.nuveq.sojibdemo.databinding.FragmentAttendanceListBinding;
+import com.nuveq.sojibdemo.utils.MyThread;
 
 public class AttendanceListFragment extends BaseFragment {
+    FragmentAttendanceListBinding binding;
+
+    MyThread thread;
 
     @Override
     protected Integer layoutResourceId() {
@@ -12,6 +21,8 @@ public class AttendanceListFragment extends BaseFragment {
 
     @Override
     protected void initFragmentComponents() {
+        binding = (FragmentAttendanceListBinding) getBinding();
+        thread = new MyThread(getActivity());
 
     }
 
@@ -23,5 +34,24 @@ public class AttendanceListFragment extends BaseFragment {
     @Override
     protected void initFragmentListener() {
 
+        binding.cardLicenceNo.setOnClickListener(view -> {
+            Runnable mRunnable;
+            Handler mHandler = new Handler();
+            mRunnable = new Runnable() {
+                @Override
+                public void run() {
+                    Toast.makeText(getActivity(), "thread", Toast.LENGTH_SHORT).show();
+                }
+            };
+            mHandler.postDelayed(mRunnable, 2 * 1000);
+
+        });
+    }
+
+
+    @Override
+    public void onDestroy() {
+        thread.interrupt();
+        super.onDestroy();
     }
 }
