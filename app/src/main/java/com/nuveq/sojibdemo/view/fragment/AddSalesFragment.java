5b327cc5ad81e7f60d6126f71f63f71e8293d45e
mdmasum.shuvo <mdmasum.shuvo.cse@gmail.com>
2019-10-27 12:07:43 +0600

    c

---
+++ app/src/main/java/com/nuveq/sojibdemo/view/fragment/AddSalesFragment.java
@@ -0,0 +1,26 @@
+package com.nuveq.sojibdemo.view.fragment;
+
+import com.nuveq.sojibdemo.R;
+import com.nuveq.sojibdemo.common.BaseFragment;
+
+public class AddSalesFragment extends BaseFragment {
+    @Override
+    protected Integer layoutResourceId() {
+        return R.layout.fragment_add_sales;
+    }
+
+    @Override
+    protected void initFragmentComponents() {
+
+    }
+
+    @Override
+    protected void initFragmentFunctionality() {
+
+    }
+
+    @Override
+    protected void initFragmentListener() {
+
+    }
+}
